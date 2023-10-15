b69ceec0903f57e0f6c13c86a6f103767f63216f
Jordan Haynes <88909614+JordanCautious@users.noreply.github.com>
2023-10-15 06:56:07 -0400

    First lines of C++ code. Seems to definitely be an increase in code complexity in exchange for higher performance.

--- HelloCPlusPlus/main.cpp
+++ HelloCPlusPlus/main.cpp
@@ -5,10 +5,29 @@
 //  Created by Jordan Haynes on 10/15/23.
 //
 
+//#include <iostream>
+//
+//int main(int argc, const char * argv[]) {
+//    // insert code here...
+//    std::cout << "Hello, World!\n";
+//    std::cout << "My name is Jordan\n";
+//    return 0;
+//}
+
 #include <iostream>
 
-int main(int argc, const char * argv[]) {
-    // insert code here...
-    std::cout << "Hello, World!\n";
+using namespace std;
+
+int main() {
+    int a = 1;
+    int b = 2;
+    // Swapping the values
+    int temp = a;
+    a = b;
+    b = temp;
+    // Should be 2
+    cout << a;
+    
     return 0;
 }
+
