9c483f01e61800520a26bdf49011879b82ca992d
Jordan Haynes <88909614+JordanCautious@users.noreply.github.com>
2023-10-15 06:31:08 -0400

    Initial Commit

---
+++ HelloCPlusPlus/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  HelloCPlusPlus
+//
+//  Created by Jordan Haynes on 10/15/23.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
